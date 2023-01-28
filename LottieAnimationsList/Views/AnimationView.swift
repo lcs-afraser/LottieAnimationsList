6ba90a1289ec631e4f90b37b78844d5405b1a48f
Alistair Fraser <afraser@lcs.on.ca>
2023-01-28 10:04:52 -0500

    Organized project and created a list view to show interesting animations.

---
+++ LottieAnimationsList/Views/AnimationView.swift
@@ -0,0 +1,20 @@
+//
+//  AnimationView.swift
+//  LottieAnimationsList
+//
+//  Created by Alistair Fraser on 2023-01-28.
+//
+
+import SwiftUI
+
+struct AnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationView()
+    }
+}
