5e55b915fce83857873cbfc61725c70698508344
Alistair Fraser <afraser@lcs.on.ca>
2023-01-28 10:30:00 -0500

    Added a structure and an array to describe my favourite animations.

---
+++ LottieAnimationsList/Model/FavouriteAnimation.swift
@@ -0,0 +1,22 @@
+//
+//  FavouriteAnimation.swift
+//  LottieAnimationsList
+//
+//  Created by Alistair Fraser on 2023-01-28.
+//
+
+import Foundation
+
+struct FavouriteAnimation: Identifiable {
+    let id = UUID()
+    let fileName: String
+    let description: String
+}
+
+let favourites = [
+    FavouriteAnimation(fileName: "130334-sweet-run-cycle", description: "A cute ball running")
+    
+    FavouriteAnimation(fileName: "132375-hypercube", description: "3 Cubes rotationg")
+    
+    FavouriteAnimation(fileName: "133580-rocket", description: "A rocket taking off")
+]
